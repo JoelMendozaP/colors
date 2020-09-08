600019ae52192d863888b82885628e61b62228db
Fernando Jerez <fernandojerez@gmail.com>
2020-09-08 16:44:21 -0500

    add link route in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import 'styles/components/Header.scss';
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import { Link } from '@reach/router';
 
 import ThemeChooser from "components/ThemeChooser"
+import { home } from 'helpers/router';
 
 export default function Header({ showBack, children }) {
     return (
@@ -14,7 +16,9 @@ export default function Header({ showBack, children }) {
                     <div className="colors-header__showback" onClick={() => window.history.back()}>
                         <FontAwesomeIcon icon={faArrowAltCircleLeft} size="lg" />
                     </div>)}
-                <div className="colors-header__title">Colors</div>
+                <Link to={home()} className="colors-header__link">
+                    <div className="colors-header__title">Colors</div>
+                </Link>
                 <div className="colors-header__extra">
                     {children}
                 </div>
--- src/helpers/router.js
+++ src/helpers/router.js
@@ -3,6 +3,10 @@ export function info_url(color){
     return path(`/info/${encodeURIComponent(color)}`);
 }
 
+export function home(color){
+    return path(`/`);
+}
+
 export function path(path){
     return process.env.PUBLIC_URL + path;
 }
