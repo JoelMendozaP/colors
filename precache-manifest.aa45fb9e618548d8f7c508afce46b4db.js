d18e7031ddd44b4a28f11736ed271b711d423010
Fernando Jerez <fernandojerez@gmail.com>
2020-09-08 12:40:44 -0500

    Updates

---
+++ precache-manifest.aa45fb9e618548d8f7c508afce46b4db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a00b20479eebf7a8c5f43d9dc6e3081",
+    "url": "/colors/index.html"
+  },
+  {
+    "revision": "83a6c98975f33176ff64",
+    "url": "/colors/static/css/main.eaf67888.chunk.css"
+  },
+  {
+    "revision": "e6ca861c694aa442b353",
+    "url": "/colors/static/js/2.581ace2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/colors/static/js/2.581ace2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a6c98975f33176ff64",
+    "url": "/colors/static/js/main.d2c7891a.chunk.js"
+  },
+  {
+    "revision": "b21169911627de49c135",
+    "url": "/colors/static/js/runtime-main.4f478495.js"
+  }
+]);
