59300126a868c3548a1a8f4d049d84de3b458446
Fernando Jerez <fernandojerez@gmail.com>
2020-09-11 14:19:33 -0500

    implemented SPA on Github Pages

--- src/helpers/router.js
+++ src/helpers/router.js
@@ -1,12 +1,28 @@
-
-export function info_url(color){
+export function info_url(color) {
     return path(`/info/${encodeURIComponent(color)}`);
 }
 
-export function home(color){
+export function home(color) {
     return path(`/`);
 }
 
-export function path(path){
+export function path(path) {
     return process.env.PUBLIC_URL + path;
+}
+
+export function setup_router(navigate) {
+    let l = window.location;
+    if (l.search) {
+        var q = {};
+        l.search.slice(1).split('&').forEach(function (v) {
+            var a = v.split('=');
+            q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
+        });
+        if (q.p !== undefined) {
+            let path = (l.pathname.endsWith('/') ? l.pathname.slice(0, -1) : l.pathname) + (q.p || '') +
+                (q.q ? ('?' + q.q) : '') +
+                l.hash;
+            navigate(path);
+        }
+    }
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import 'styles/index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from "@reach/router"
+import { Router, navigate } from "@reach/router"
 import { Provider } from 'react-redux';
-import { createBrowserHistory } from 'history';
 
 import Store from 'models/store';
-import { path } from 'helpers/router';
+import { path, setup_router } from 'helpers/router';
 
 import Home from 'views/Home';
 import ColorInfo from 'views/ColorInfo';
 
-export const history = createBrowserHistory({
-    basename: process.env.PUBLIC_URL
-});
+setup_router(navigate);
 
 ReactDOM.render(
   <Provider store={Store}>
